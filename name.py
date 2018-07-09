7ec847c9e1886d0a91e24eb01f392467017552bc
vishnu <vishnuprakash406@gmail.com>
2018-07-09 11:34:16 +0530

    get the name of stocks

---
+++ name.py
@@ -0,0 +1,12 @@
+a1=['i1','pen',100,5]
+a2=['i2','pencil',8,5]
+a3=['i3','rice',50,50]
+a=[a1,a2,a3]
+l=len(a)
+print(' ')
+print('--------------')
+print ('name       ')
+print('--------------')
+for j in range(0,l) :
+	print(' ')
+	print a[j][1]
