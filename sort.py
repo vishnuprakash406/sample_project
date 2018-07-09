726dfde7bca1fe815856bc06080475447d531c03
vishnu <vishnuprakash406@gmail.com>
2018-07-09 14:27:03 +0530

    modified sorting

--- sort.py
+++ sort.py
@@ -4,12 +4,7 @@ for i in range(0,n):
       temp=input("enter the number to array to be sorted: ")
       arr.append(temp)
 print("the unsorted array is",arr)
-for i in range(0,n):
-    for j in range(0,n-i-1):
-        if(arr[j]>arr[j+1]):
-            var=arr[j]
-            arr[j]=arr[j+1]
-            arr[j+1]=var
-print("the sorted array is")
-for i in range(0,n):
-    print arr[i]   
+arr.sort()
+print ("sorted list is : ",arr)
+
+
